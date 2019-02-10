1b945899fb53339cac05a2d3ef610855d14c81ce
Giulio Rossi <ciuffi9@mac.com>
2019-02-10 13:47:06 -0800

    Allow enter search

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="searchBar">
         <div class="input-group mb-3">
-            <input type="text" class="form-control" v-model="searchTerm" placeholder="Enter keywords here">
+            <input v-on:keyup.enter="$emit('search', searchTerm)" type="text" class="form-control" v-model="searchTerm" placeholder="Enter keywords here">
             <div class="input-group-append">
                 <button class="btn btn-outline-secondary" type="button" id="button-addon2" v-on:click="$emit('search', searchTerm)">Search</button>
             </div>
