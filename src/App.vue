ed179209063aeba7222c7470d4253721d04d5a97
Giulio Rossi <ciuffi9@mac.com>
2019-02-09 22:45:07 -0800

    Add homepage

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <toolbar v-on:scroll-down="scrollToClass"></toolbar>
+    <img class="logo" alt="acaMEDia logo" src="./assets/logo.png">
+    <home></home>
+  </div>
+</template>
+
+<script>
+
+  import Home from "./Containers/Home";
+  const scroller = {
+    methods: {
+      scrollToClass(className) {
+        // Get the first element with the given class name
+        var el = this.$el.getElementsByClassName(className)[0];
+        // Get the bounding rectangle so we can get the element position position
+        let rect = el.getBoundingClientRect()
+        // Scroll to the element (using x and y axis)
+        window.scrollTo(rect.left, rect.top)
+      }
+    }
+  };
+  import Toolbar from './components/toolbar.vue'
+  export default {
+    name: 'app',
+    mixins: [scroller],
+    components: {
+      Home,
+      Toolbar
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/locationBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="locationBar">
+        <label for="select">Location filter: </label>
+        <select id="select"
+                v-bind:value="locationSelection"
+                v-on:input="$emit('updateOptions', $event.target.value)">
+            <option disabled value="">Please select one</option>
+            <option v-for="option in locationOptions" v-bind:value="option">
+                {{option}}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+
+    const locationOptions = [
+        "USA",
+        "Canada",
+        "Mexico",
+        "Italy",
+        "Nippon"
+    ]
+    export default {
+        name: "locationBar",
+        props: ["locationSelection"],
+        data: function () {
+            return {
+                locationOptions: locationOptions,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
