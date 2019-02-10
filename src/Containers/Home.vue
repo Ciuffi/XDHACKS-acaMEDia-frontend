6567bd5353394af6aa5883baf3079c90164a92aa
allisonchiang <a8s1b@ugrad.cs.ubc.ca>
2019-02-10 05:56:48 -0800

    finish about animations and formatting ish

--- src/Containers/Home.vue
+++ src/Containers/Home.vue
@@ -7,34 +7,64 @@
                 <location-bar v-on:updateOptions="updateLocationHandler"></location-bar>
             </div>
         </div>
-        <homeContent class="about" title="About">
-            <div>
-                <p>
-                    acaMEDia is a vue.js-based webapp geared towards undergraduate and graduate Science students
-                    looking to gain more knowledge about current medical research papers and the flourishing scientific
-                    laboratories conducting the research. The world of scientific academia can be daunting to traverse,
-                    with many laboratories around the world and individual laboratory websites to explore.
-                </p>
+
+        <homeContent class="about" style="margin-top: 20%; margin-left: auto; margin-right: auto;" title="About">
+            <div id="about-stuff">
+                <div class="about-row-container">
+                    <div id="about-one">
+                        <p>
+                            acaMEDia is a vue.js-based webapp geared towards undergraduate and graduate Science students
+                            looking to gain more knowledge about current medical research papers and the flourishing scientific
+                            laboratories conducting the research.
+                        </p>
+                        <p>
+                            The world of scientific academia can be daunting to traverse,
+                            with many laboratories around the world and individual laboratory websites to explore.
+                        </p>
+                    </div>
+                    <div id="chair-container">
+                        <img id="chair" src="../assets/acamedia-chair.png">
+                    </div>
+                </div>
+                <br style="clear: both;"/>
+                <div id="beaker-container">
+                    <img id="beaker" src="../assets/acamedia-beaker.png">
+                </div>
+                <div id="about-two">
+                    <p>
+                        Whether it's an undergraduate student looking to kickstart their lifelong passion in a scientific
+                        specialization or it's a graduate student looking to contribute towards a lab with publications
+                        that align to their interests, we are confident that acaMEDia can help bridge the gap and bring
+                        more young minds in contact with medical research.
+                    </p>
+                </div>
+                <br style="clear: both;"/>
+                <div id="about-three">
+                    <p>
+                        A user can access the home page and enter keywords for their interested field of study in the search bar,
+                        optionally also entering a location filter for country and city of potential lab results.
+                    </p>
+                    <p>
+                        Upon starting the search,
+                        the user will be directed to a page of search results comprised of labs with the most publications related to their search query,
+                        and the user may read more about the lab and its research by clicking on the result name.
+                    </p>
+                    <p>
+                        The user can then access the lab profile,
+                        which consists of recent publications, the lab's website, and the lab's focus.
+                    </p>
+                </div>
+                <div id="octocat-container">
+                    <img id="octocat" src="../assets/acamedia-octocat.png">
+                </div>
+                <br style="clear: both;"/>
             </div>
-            <div>
-                <p>
-                    Whether it's an undergraduate student looking to kickstart their lifelong passion in a scientific
-                    specialization or it's a graduate student looking to contribute towards a lab with publications
-                    that align to their interests, we are confident that acaMEDia can help bridge the gap and bring
-                    more young minds in contact with medical research.
-                </p>
+            <div class="container" id="bottom-shelf-container">
+                <img id="bottom-shelf-img" class="logo" alt="acaMEDia logo" src="../assets/acamedia-bottom-shelf.png">
             </div>
-            <div>
-                <p>
-                    A user can access the home page and enter keywords for their interested field of study in the search bar,
-                    optionally also entering a location filter for country and city of potential lab results. Upon starting the search,
-                    the user will be directed to a page of search results comprised of labs with the most publications related to their search query,
-                    and the user may read more about the lab and its research by clicking on the result name. The user can then access the lab profile,
-                    which consists of recent publications, the lab's website, and the lab's focus.
-                </p>
-            </div>
+            <br style="clear: both;"/>
         </homeContent>
-        <homeContent class="contact" title="Contact">
+        <homeContent class="contact" style="margin-top: 20%; margin-left: auto; margin-right: auto;" title="Contact">
             <div>
                 <p>
                     Created by <a href="https://www.linkedin.com/in/allison-chiang">Allison Chiang</a>, <a href="https://www.linkedin.com/in/austinkobayashi">Austin Kobayashi</a>  and <a href="https://www.linkedin.com/in/giulio-rossi-662152110">Giulio Rossi</a>
@@ -72,11 +102,13 @@
     .home{
         background-color: #d8e5f8;
         color: #6c6c77;
+        padding: 20px;
     }
     #header-container{
         position:relative;
+        margin-top: 12%;
     }
-    #header-img{
+    #header-img, #bottom-shelf-img{
         width:100%;
         margin-top: 20px;
         margin-left: auto;
@@ -88,5 +120,94 @@
         bottom: 10px;
         width: 70%;
     }
+    #bottom-shelf-container{
+        position: inherit;
+    }
+    #about-one {
+        margin: 70px;
+        width: 40%;
+        float: left;
+        height: auto;
+    }
+    #chair-container{
+        width: 40%;
+        float: right;
+        padding: 2px;
+        position: relative;
+    }
+    #chair{
+        height: 300px;
+        position: absolute;
+        left: 0;
+        x-transition: all 1.5s ease-in-out;
+        animation: aniside 2.5s infinite;
+    }
 
+    @keyframes aniside {
+        0% {
+            left: 0;
+        }
+        50% {
+            left: 80px;
+        }
+        100% {
+            left: 0;
+        }
+    }
+    #beaker-container{
+        width: 40%;
+        float: left;
+        padding: 2px;
+        position: absolute;
+    }
+    #beaker{
+        height: 250px;
+        padding-left: 30px;
+        position: absolute;
+        left: 0;
+    }
+    @keyframes aniup {
+        0% {
+            top: 0;
+        }
+        50% {
+            top: 50px;
+        }
+        100% {
+            top: 0;
+        }
+    }
+    #about-two {
+        display: inline-block;
+        margin: 70px;
+        width: 40%;
+        clear: right;
+        float: right;
+        height: auto;
+    }
+
+    #about-three {
+        display: inline-block;
+        margin: 70px;
+        width: 40%;
+        clear: right;
+        height: auto;
+    }
+    #octocat-container{
+        width: 40%;
+        float: right;
+        padding: 2px;
+        position: relative;
+    }
+    #octocat{
+        height: 400px;
+        position: absolute;
+        margin-left: -15%;
+        left: 0;
+        x-transition: all 1.5s ease-in-out;
+        animation: aniup 2.5s infinite;
+    }
+    .contact{
+        position:relative;
+    }
 </style>
